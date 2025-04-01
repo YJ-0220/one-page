ee45407ba3f905300d1113fbdb172ccccf7ce885
YJ <sneakers1577@gmail.com>
2025-04-01 12:01:54 +0900

    cors 오류 해결중

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -66,6 +66,8 @@ app.use(
       "https://YJ-0220.github.io/one-page/",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
